d2dac55b7f3118457b26df509373b0b38baf7823
Carbophozz <57803010+Carbophozz@users.noreply.github.com>
2020-04-12 19:41:37 +0300

    Update itog.py

--- itog.py
+++ itog.py
@@ -1,57 +1,81 @@
 ﻿import numpy as np
 
-def game_core_v3(number):
-     
-    # Идем путём банальной дихотомии
-    count = 0
-    predict = 50 # Начинаем с середины диапазона
-    step = 25 # Первый шаг равен четверти диапазона
-   
 
+def game_core_v3(number):
+
+    '''using trivial diсhotomy search'''
+    
+    # just in case we might change the range of possible guesses
+    lo = 1
+    hi = 100
+    
+    count = 1
+    
+    step = ((hi-lo) + (hi-lo)%2) // 2  # initally it is half length of the range
+    predict = lo+step  # and we start checking right from the middle of the range
+    
     while number != predict:
         
-        count+=1
+        #with each iteration step is diminished in half
+        step = (step + step % 2) // 2
+
         if number > predict: 
             predict += step
+
         elif number < predict: 
             predict -= step
-        
-        #делим шаг пополам, с защитой от промахов
-        step = (step + step % 2) // 2
-    
+         
+        count+=1
+
     return(count)
 
 
-'''  то же самое в рекурсивном варианте
-     для единообразия вызова функции функцией-прогонщиком задаем дефолтные значения 
-     стартовой версии и шага
-     Правда, оно работает на 20% медленнее чем предыдущий вариант'''
+
+
+
+ #the recursive version
+ #looks much better, but is 20% slower
+ #and we have to hard preset the predict and step values
+ #so it could be called by the testing function seamlessly
+ #but I've wrote this just to check if it will be significantly slower    
+
+
 def game_core_v4(number, predict=50, step=50):
-    
-    
+
+
     step = (step + step % 2) // 2
-    
+  
     if number > predict: 
         return 1 + game_core_v4(number, predict+step, step)
-        
+     
+
     if number < predict: 
         return 1 + game_core_v4(number, predict-step, step)
-    
-    return 0
-        
-        
+   
+    return 1
+
+
 
         
+
 def score_game(game_core):
+
     '''Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
+
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=(1000))
+    random_array = np.random.randint(1,100, size=(1000))
     for number in random_array:
         count_ls.append(game_core(number))
+
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+
     return(score)
+
+
 
 score_game(game_core_v3)
 score_game(game_core_v4)
+
+
